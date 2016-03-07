e94638773719ae27c5e3f99cde903587692a2268
Amanda Milke <amandalmilke@gmail.com>
2016-03-06 19:49:56 -0500

    added concat to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
 
-gulp.task('default', function(){
+gulp.task('style', function(){
 	return gulp.src('./styles/styles.styl')
 		.pipe(stylus())
 		.pipe(gulp.dest('./styles'));
 });
+
+gulp.task('scripts', function(){
+	return gulp.src('./scripts/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('./scripts/'))
+});	
+
+gulp.task('default', ['style', 'scripts']);
